d65ce7c8ebd8f195a39d226a378233f91b72a12b
Cyberenchanter <cyberenchanter0@gmail.com>
2025-12-12 11:32:37 +0100

    Fix dependencies

--- contracts/InsuranceProtocol.sol
+++ contracts/InsuranceProtocol.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 interface IProbabilityOracle {
     function isPayoutEvent(uint256 productId) external view returns (bool);
